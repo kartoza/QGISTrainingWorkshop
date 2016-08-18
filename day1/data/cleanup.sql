d748d4b68aa8f0aea84be692a75664b69a487c58
Tim Sutton <tim@kartoza.com>
2016-08-18 07:23:07 +0200

    Initial version with day1 complete

---
+++ day1/data/cleanup.sql
@@ -0,0 +1,5 @@
+alter table ne_10m_airports drop column label_x_1mil;
+alter table ne_10m_airports drop column label_y_1mil;
+alter table ne_10m_airports drop column rotation;
+alter table ne_10m_airports drop column font;
+alter table ne_10m_airports drop column font_size_1mil;
