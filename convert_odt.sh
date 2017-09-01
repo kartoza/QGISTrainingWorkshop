5d6394d3fab3556a8aebc07a6cc975aaf9bcc7b5
admire <admire@kartoza.com>
2017-09-01 14:00:31 +0200

    added script for converting odt documents to pdf. The script will overwrite the exisitng one so that if they are new changes in the odt they will be reflected

---
+++ convert_odt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Usage ./convert_odt.sh path_to_QGIS_folder_in_repo 
+# or /bin/bash/convert_odt.sh  path_to_QGIS_folder_in_repo
+
+DATA_PATH=/home/QGISTrainingWorkshop/QGIS
+
+if [ -n "$1" ]
+then
+    DATA_PATH=$1
+fi
+
+
+for d in $(find ${DATA_PATH} -maxdepth 1 -type d);
+    do
+    pushd $d &&  libreoffice --headless --convert-to pdf *.odt && popd;
+done
+
+
+
